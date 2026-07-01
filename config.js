a81a767913c53ff310413bdff3ad70f6d20970c4
Cryptovaultiq <hollyhub146@gmail.com>
2026-07-01 21:45:24 +0100

    Update Web3Forms API key in config.js

--- config.js
+++ config.js
@@ -3,4 +3,4 @@
 // For local development: create a local copy of this file with your key
 // For Vercel: update the key value below or use environment variables
 
-window.__WEB3FORMS_KEY__ = 'b5f9f926-ecd5-4757-b0ad-ff1954bd43ea';
+window.__WEB3FORMS_KEY__ = '1efaad3c-90c4-462d-83e7-eb60307df491';
